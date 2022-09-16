fc07a2c282051d1df5ef2a40183c2e0fcef21f95
Eduardo Ayerve Cruz | 에두아르도 <eduardo.ayerve@gmail.com>
2022-09-16 18:23:42 +0900

    Update pa1.c

--- pa1.c
+++ pa1.c
@@ -1,3 +1,22 @@
+//---------------------------------------------------------------
+//
+//  4190.308 Computer Architecture (Fall 2022)
+//
+//  Project #1:
+//
+//  September 6, 2022
+//
+//  Seongyeop Jeong (seongyeop.jeong@snu.ac.kr)
+//  Jaehoon Shim (mattjs@snu.ac.kr)
+//  IlKueon Kang (kangilkueon@snu.ac.kr)
+//  Wookje Han (gksdnrwp@snu.ac.kr)
+//  Jinsol Park (jinsolpark@snu.ac.kr)
+//  Systems Software & Architecture Laboratory
+//  Dept. of Computer Science and Engineering
+//  Seoul National University
+//
+//---------------------------------------------------------------
+
 typedef unsigned char u8;
 
 /* TODO: Implement this function */
